2c22761c9b51ab075e8cbe52e6cb2634f4fb9ffc
slingvector <iitr.anuj.wrk@gmail.com>
2026-03-31 23:41:49 +0530

    chore: bump version to 1.0.1 for patch release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="linkedin-write-flow",
-    version="1.0.0",
+    version="1.0.1",
     description="A robust, SOLID Python package for publishing content on LinkedIn via OAuth 2.0.",
     author="Your Name",
     packages=find_packages(exclude=["tests*"]),
